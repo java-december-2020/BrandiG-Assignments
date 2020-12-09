dcc56003bc54e9190ae6f24dafe00600a83ca81e
brandigreene <72356558+brandigreene@users.noreply.github.com>
2020-12-08 20:08:11 -0600

    Add files via upload

---
+++ 01-java-fundamentals/02-pythagorean-theorem/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest {
+    public static void main (String[] agrs){
+        Pythagorean id = new Pythagorean();
+        double hypotenuse = id.calculateHypotenuse(5,6);
+        System.out.println(hypotenuse);
+    }
+}
